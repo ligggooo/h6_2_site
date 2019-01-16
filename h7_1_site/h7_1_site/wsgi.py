1daf5186be69bf7548e729376a8b03a01b348586
ligggooo <ligggooowow@163.com>
2019-01-16 11:13:26 +0800

    7_1 CRM项目创建

---
+++ h7_1_site/h7_1_site/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for h7_1_site project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'h7_1_site.settings')
+
+application = get_wsgi_application()
